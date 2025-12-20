26c33624ebc747cfec8827c5f16de50777a4d743
unknown <roy123123sohat@gmail.com>
2025-12-20 20:31:55 +0200

    read/write Ptr func

--- examples/test.cpp
+++ examples/test.cpp
@@ -18,19 +18,37 @@ struct player{
 int main(){
 
     player player1;    
+    int* ptr;
+    size_t count;
 
     Serialization::Deserializer dese;
     dese.load_from_file("data.bin");
     dese.read(player1);
+    dese.read_ptr(ptr, count);
 
     std::cout << "id: " << player1.id << ", number: " << player1.number << std::endl;
+
+    player1.number = 123;
     player1.id = 'b';
 
     Serialization::Serializer seri;
 
     seri.write(player1);
 
+
+    ptr = new int[5]{104,101,108,108,111};
+
+    seri.write_Ptr(ptr, count);
+
+    delete[] ptr;
+
+
+    std::string str = "hello";
+    seri.write_Ptr(str.data(), str.length());
+
+
     seri.save_to_file("data.bin");
+    // if u save only player:
     // with a quick ls -l data.bin you can see that the size is 5 bytes
     // even tho if you know check the sizeof(player) it will say 8
     // this weird behavior is caused by padding between the char and the int
--- include/serialization.hpp
+++ include/serialization.hpp
@@ -31,13 +31,33 @@ namespace Serialization{
             buffer.insert(buffer.end(), ptr, ptr+sizeof(T));
         }
 
-        //for non-fundamental types
+        //for struct/class types
         //doing that cause struct/class have hidden padding for cpu perposes
         //but this data shell not be stored for security/efficency reasons
         
         template <typename T>
         requires Serializable<T, Serializer>
         void write(T& value){ value.serialize(*this); }
+
+        //for pointer types
+        template <typename T>
+        requires std::is_pointer_v<T>
+        void write_Ptr(T ptr, size_t count){
+            // writing the size first so the desrializer knows how much to read
+            uint32_t ucount = static_cast<uint32_t>(count);
+            write(ucount);
+
+            using baseType = std::remove_pointer_t<T>;
+            if(std::is_fundamental_v<baseType>){
+                uint8_t* rawData = reinterpret_cast<uint8_t*>(ptr);
+                buffer.insert(buffer.end(), rawData, rawData+(count*sizeof(baseType)));
+            }
+            else{
+                for(int i=0; i<count; ++i){
+                    write(ptr[i]);
+                }
+            }
+        }
 
         // now save is file is in here based on oop, but may for more flexable use i will move it to be a non member
         void save_to_file(const std::string&);
@@ -63,6 +83,35 @@ namespace Serialization{
         requires Deserializable<T, Deserializer>
         void read(T& value){
             value.deserialize(*this);
+        }
+
+        template <typename T>
+        requires std::is_pointer_v<T>
+        // this function will also allocate space for the data
+        void read_ptr(T& ptr, size_t& count){
+            uint32_t countRef;
+            read(countRef);
+            count = countRef;
+
+            using baseType = std::remove_pointer_t<T>;
+
+            ptr = new baseType[count];
+
+            if constexpr (std::is_fundamental_v<baseType>) {
+                    // fast path: Block copy
+                    size_t totalBytes = count * sizeof(baseType);
+                    if (offset + totalBytes > buffer.size()) throw std::runtime_error("Overflow boho");
+        
+                    std::memcpy(ptr, &buffer[offset], totalBytes);
+                    offset += totalBytes;
+            }
+            else {
+                // slow path: Deserialize into each new slot
+                for (size_t i = 0; i < count; ++i) {
+                    read(ptr[i]);
+                }
+            }
+
         }
 
 
